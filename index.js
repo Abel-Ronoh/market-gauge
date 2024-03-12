9cc02614ae63e20e8c12b37878f5c6a874ba76b7
Abel-Ronoh <abellronoh@gmail.com>
2024-03-12 13:40:23 +0300

    created mobile layout

--- index.js
+++ index.js
@@ -25,36 +25,42 @@ let currentDataIndex = 0;
 sellCounter.style.color = 'red'
 buyCounter.style.color = 'blue'
 holdCounter.style.color = 'orange'
+var timer = 0
 
 function updateTicker() {
     const currentData = datas[currentDataIndex];
     // tickerText.textContent = `${currentData.symbol}`;
     tickerValue.textContent = currentData.value;
-
+    
     // Update gauge based on value
     gaugeNeedles.classList.remove("buy", "sell", 'strongbuy', "strongsell");
     if (currentData.value === "STRONG BUY") {
         tickerValue.style.color = "blue"
         gaugeNeedles.classList.add("strongbuy");
+        timer = 150000
         counterBuy +=1
         buyCounter.textContent = counterBuy
     } else if (currentData.value === "SELL") {
         tickerValue.style.color = "red"
         gaugeNeedles.classList.add("sell");
+        timer = 150000
         counterSell +=1
         sellCounter.textContent = counterSell
     }else if (currentData.value == "HOLD"){
         tickerValue.style.color = "#5C5C5C"
+        timer = 3000
         counterHold +=1
         holdCounter.textContent = counterHold
     }else if (currentData.value === "BUY") {
         tickerValue.style.color = "blue"
         gaugeNeedles.classList.add("buy");
+        timer = 150000
         counterBuy +=1
         buyCounter.textContent = counterBuy
     }else if (currentData.value === "STRONG SELL") {
         tickerValue.style.color = "red"
         gaugeNeedles.classList.add("strongsell");
+        timer = 150000
         counterSell +=1
         sellCounter.textContent = counterSell
     }
@@ -64,6 +70,6 @@ function updateTicker() {
 }
 
 
-setInterval(updateTicker, 3000); // Update ticker every 3 seconds
+setInterval(updateTicker, 10000); // Update ticker every 3 seconds
 
 updateTicker(); // Initial update
